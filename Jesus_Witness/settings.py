1d365bba0cd8db82e4a62fbb3ff3c284b71e6d1f
Franklin Obasi <franklinobasy@gmail.com>
2023-03-14 14:50:14 +0100

    Jesus saves

--- Jesus_Witness/settings.py
+++ Jesus_Witness/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "home",
+    "blog",
+    "witness",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
